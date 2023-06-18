b401fb2927621187b16460d2e05ce9184ba9d119
VikaVashchuk <viktoriavashchuk@gmail.com>
2023-06-18 19:50:39 +0300

    movie_picker (task 16)

--- week_3/movie_picker/movie_picker.py
+++ week_3/movie_picker/movie_picker.py
@@ -10,19 +10,29 @@ GENRES = {
 }
 
 
-ACTORS = {
-    'Robert De Niro': ['Meet the Parents'],
-    'Ben Stiller': ['Meet the Parents'],
-    'Adam Sandler': ['Anger Management'],
-    'Jack Nicholson': ['Anger Management'],
-    'Brendan Fraser': ['Mummy'],
-    'Rachel Weisz': ['Mummy'],
-    'Tom Cruise': ['Vanilla Sky', 'Mission Impossible'],
-    'Penelope Cruz': ['Vanilla Sky'],
-    'Cameron Diaz': ['Vanilla Sky'],
-    'Brad Pitt': ['Meet Joe Black'],
-    'Anthony Hopkins': ['Meet Joe Black'],
-    'Jeremy Renner': ['Mission Impossible']
+#ACTORS = {
+#    'Robert De Niro': ['Meet the Parents'],
+#    'Ben Stiller': ['Meet the Parents'],
+#    'Adam Sandler': ['Anger Management'],
+#    'Jack Nicholson': ['Anger Management'],
+#    'Brendan Fraser': ['Mummy'],
+#    'Rachel Weisz': ['Mummy'],
+#    'Tom Cruise': ['Vanilla Sky', 'Mission Impossible'],
+#    'Penelope Cruz': ['Vanilla Sky'],
+#    'Cameron Diaz': ['Vanilla Sky'],
+#    'Brad Pitt': ['Meet Joe Black'],
+#    'Anthony Hopkins': ['Meet Joe Black'],
+#    'Jeremy Renner': ['Mission Impossible']
+#}
+
+
+CAST = {
+    'Meet the Parents': ['Robert De Niro', 'Ben Stiller'],
+    'Anger Management': ['Adam Sandler', 'Jack Nicholson'],
+    'Mummy': ['Brendan Fraser', 'Rachel Weisz'],
+    'Vanilla Sky': ['Tom Cruise', 'Penelope Cruz', 'Cameron Diaz'],
+    'Meet Joe Black': ['Brad Pitt', 'Anthony Hopkins'],
+    'Mission Impossible': ['Tom Cruise', 'Jeremy Renner']
 }
 search_by_genre = input("Search by Genre? (y/n): ")
 if search_by_genre.lower() == 'y':
@@ -41,14 +51,34 @@ if search_by_genre.lower() == 'y':
 else:
     print("Invalid input. Please try again.")
 
+#search_by_genre = input("Search by Genre? (y/n): ")
+#if search_by_genre.lower() == 'n':
+#    search_by_actor = input("Search by Actor? (y/n): ")
+#   if search_by_actor.lower() == 'y':
+#        print("Available Actors:", list(ACTORS.keys()))
+#        actor = input("Enter actor: ")
+#        if actor in ACTORS:
+#            movies = ACTORS[actor]
+#            print("Available movies:", movies, "with", actor)
+#            movie = input("Enter movie: ")
+#            if movie in movies:
+#                print("Movie to watch:", movie + ". Starring:", actor + ".")
+#            else:
+#                print("Movie not found.")
+#        else:
+#            print("Invalid actor.")
+#    else:
+#        print("Invalid input. Please try again.")
+
 search_by_genre = input("Search by Genre? (y/n): ")
 if search_by_genre.lower() == 'n':
     search_by_actor = input("Search by Actor? (y/n): ")
     if search_by_actor.lower() == 'y':
-        print("Available Actors:", list(ACTORS.keys()))
+        actors = set(actor for movie in CAST for actor in CAST[movie])
+        print("Available Actors:", list(actors))
         actor = input("Enter actor: ")
-        if actor in ACTORS:
-            movies = ACTORS[actor]
+        movies = [movie for movie in CAST if actor in CAST[movie]]
+        if movies:
             print("Available movies:", movies, "with", actor)
             movie = input("Enter movie: ")
             if movie in movies:
@@ -56,9 +86,10 @@ if search_by_genre.lower() == 'n':
             else:
                 print("Movie not found.")
         else:
-            print("Invalid actor.")
+            print("No movies found for", actor)
     else:
         print("Invalid input. Please try again.")
+
 
 
 
