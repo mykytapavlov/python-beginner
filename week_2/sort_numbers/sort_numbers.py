dfd32c5bbaf31e1f90ef2ccf714835a27c8b15af
Olena lesyk <osafo1985@gmail.com>
2023-06-18 22:39:07 +0200

    add sorting of the numbers in the list

--- week_2/sort_numbers/sort_numbers.py
+++ week_2/sort_numbers/sort_numbers.py
@@ -1,2 +1,9 @@
 if __name__ == '__main__':
     print('Task 10. Sort numbers')
+txt = input('Enter several numbers with random spacing between each (no comma needed): ')
+x = txt.split()
+print('x: ', x, id(x))
+y = [int(item) for item in x]
+y.sort()
+print('y: ', y, id(y))
+# READY
