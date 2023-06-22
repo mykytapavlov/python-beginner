5363683169164f935ab4b67e70f5c4d9f6368766
Olena lesyk <osafo1985@gmail.com>
2023-06-22 13:50:07 +0200

    each third (task 11)

--- week_2/each_third/each_third.py
+++ week_2/each_third/each_third.py
@@ -1,2 +1,10 @@
 if __name__ == '__main__':
     print('Task 11. Each third')
+txt = input('Enter sequence of values with random spacing between each (no comma needed): ')
+x = txt.split()
+print('x: ', x, id(x))
+y = x[::3]
+print(y)
+#you can use select all => add step
+#list
+#for now leave as it is
