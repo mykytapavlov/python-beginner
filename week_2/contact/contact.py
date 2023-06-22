019c4656077784bf41ca0c68d40cf9039ca5e93e
Olena lesyk <osafo1985@gmail.com>
2023-06-22 13:54:51 +0200

    contact (task 14)

--- week_2/contact/contact.py
+++ week_2/contact/contact.py
@@ -1,2 +1,10 @@
 if __name__ == '__main__':
     print('Task 14. Contact.')
+x = input('Enter your name: ')
+y = input('Enter your age: ')
+z = input('Enter your address: ')
+d = input('Enter your phone: ')
+my_dict = dict(name=x, age=y, address=z, phone=d)
+print('Contact created: ', my_dict)
+#dict, do not use name dict
+#ready
