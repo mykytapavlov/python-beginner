fec2543dc63f29d6e9971870f805fc9024e37f73
Olena lesyk <osafo1985@gmail.com>
2023-06-22 13:52:43 +0200

    equal parts (task 12)

--- week_2/equal_parts/equal_parts.py
+++ week_2/equal_parts/equal_parts.py
@@ -1,2 +1,10 @@
 if __name__ == '__main__':
     print('Task 12. Equal parts')
+txt = input('Enter sequence of 8 values with random spacing between each (no comma needed): ')
+x = txt.split()
+print('x: ', x, id(x))
+y = x[0:4]
+d = x[4:]
+print(y, d)
+#list
+#ready
