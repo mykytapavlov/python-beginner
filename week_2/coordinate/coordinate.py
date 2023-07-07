08a5afd05c6630eb8e2c07a9c83ee7c609e751e5
Olena lesyk <osafo1985@gmail.com>
2023-07-07 09:23:41 +0200

    coordinate (task 13)

--- week_2/coordinate/coordinate.py
+++ week_2/coordinate/coordinate.py
@@ -1,2 +1,12 @@
 if __name__ == '__main__':
     print('Task 13. Coordinate.')
+x = input('Enter x: ')
+y = input('Enter y: ')
+z = input('Enter z: ')
+x = int(x)
+y = int(y)
+z = int(z)
+g = (x, y, z)
+print('Coordinate ', g, type(x), type(y), type(z), id(g))
+#tuple and integers
+#ready
