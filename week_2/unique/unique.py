8504c172723d38e9807da49674e657b747404a2a
olena lesyk <osafo1985@gmail.com>
2023-07-12 12:18:35 +0200

    unique (task 15)

--- week_2/unique/unique.py
+++ week_2/unique/unique.py
@@ -1,2 +1,14 @@
 if __name__ == '__main__':
     print('Task 15. Unique.')
+my_txt = input('Enter the sequence of numbers with random comma: ')
+x = my_txt.replace(',', ' ')
+y = set(x.split())
+z = list(y)
+a = [int(item) for item in z]
+b = sorted(a)
+c = [str(item) for item in b]
+print(', '.join(c))
+for item in my_txt:
+    print(item, my_txt.count(item))
+
+#ready
