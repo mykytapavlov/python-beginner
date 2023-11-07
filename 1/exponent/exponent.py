1c5329d66968299bf2bcb434922f7a5a4512571a
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-07 22:58:09 +0200

    add program which returns exponent of two numbers

--- 1/exponent/exponent.py
+++ 1/exponent/exponent.py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
     x_var = input("Enter value for x: ")
     y_var = input("Enter value for y: ")
-    print("Sum of x+y is equal to " + str(int(x_var) ** int(y_var)))
+    print("Exponent of x+y is equal to " + str(int(x_var) ** int(y_var)))
