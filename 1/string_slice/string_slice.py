6fd3162a6e43bbe4c6970ebdac0fba94cd1a64f8
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-07 23:24:43 +0200

    add program which slice string~

--- 1/string_slice/string_slice.py
+++ 1/string_slice/string_slice.py
@@ -1,2 +1,5 @@
 if __name__ == '__main__':
-    print('Task 7. String slice')
+    var_str = input("Please enter your text: ")
+    var_start = int(input("Please enter start index: "))
+    var_end = int(input("Please enter end index: "))
+    print("Result of index operation is: \n" + var_str[var_start:var_end])
