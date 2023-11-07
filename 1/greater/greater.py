444527c4e8507058dc956b43f2a2616f3f11fbdc
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-07 23:33:52 +0200

    check if number is greater

--- 1/greater/greater.py
+++ 1/greater/greater.py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
     var_x = int(input("Please enter x: "))
     var_y = int(input("Please enter y: "))
-    print("Is y grater than x? =  " + str(var_y > var_x))
+    print("Is y greater than x? =  " + str(var_y > var_x))
