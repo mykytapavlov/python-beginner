968ebba48d5adb8d148116d30d31193b10ce285a
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-07 22:51:46 +0200

    add program which sums two numbers

--- 1/sum_numbers/sum_numbers.py
+++ 1/sum_numbers/sum_numbers.py
@@ -1,2 +1,4 @@
 if __name__ == '__main__':
-    print('Task 3. Sum numbers.')
+    x_var = input("Enter value for x: ")
+    y_var = input("Enter value for y: ")
+    print("Sum of x+y is equal to " + str(int(x_var) + int(y_var)))
