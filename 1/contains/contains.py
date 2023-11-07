17460eae92a4c075eea35d5d4504f803ee7b0ef4
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-07 23:43:38 +0200

    check if letter in word

--- 1/contains/contains.py
+++ 1/contains/contains.py
@@ -1,2 +1,5 @@
 if __name__ == '__main__':
-    print('Task 9. Contains?')
+    var_word = input("Please enter word: ")
+    var_letter = input("Please enter letter: ")
+    print("Does letter " + var_letter + "is in word " + var_word + "? = " + str(var_letter in var_word))
+    
