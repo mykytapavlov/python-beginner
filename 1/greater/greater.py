3a4ec509ffa8b26f58d350f839608446a4bcd346
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-14 23:30:20 +0200

    changed print format

--- 1/greater/greater.py
+++ 1/greater/greater.py
@@ -1,4 +1,5 @@
 if __name__ == '__main__':
     var_x = int(input("Please enter x: "))
     var_y = int(input("Please enter y: "))
-    print("Is y greater than x? =  " + str(var_y > var_x))
+    print(f'Is y greater than x? = {var_y > var_x}')
+    
