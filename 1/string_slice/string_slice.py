06b96bc5a7fd95a91bec3fb0aba490059c586dd0
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-14 23:25:36 +0200

    changed print format

--- 1/string_slice/string_slice.py
+++ 1/string_slice/string_slice.py
@@ -2,4 +2,5 @@ if __name__ == '__main__':
     var_str = input("Please enter your text: ")
     var_start = int(input("Please enter start index: "))
     var_end = int(input("Please enter end index: "))
-    print("Result of index operation is: \n" + var_str[var_start:var_end])
+    print(f'Result of index operation is: \n{var_str[var_start:var_end]}')
+    
