6768ce5a035b9741f6c2775bc14790800fbec1a6
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-14 23:35:29 +0200

    changed print format

--- 1/contains/contains.py
+++ 1/contains/contains.py
@@ -1,5 +1,4 @@
 if __name__ == '__main__':
     var_word = input("Please enter word: ")
     var_letter = input("Please enter letter: ")
-    print("Does letter " + var_letter + "is in word " + var_word + "? = " + str(var_letter in var_word))
-    
+    print(f'Does letter {var_letter} is in word {var_word} ? = {var_letter in var_word}')
