72a29bdb102e395909441f08f3ca57142ed13dd5
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-14 23:32:13 +0200

    changed print format

--- 1/exponent/exponent.py
+++ 1/exponent/exponent.py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
     x_var = input("Enter value for x: ")
     y_var = input("Enter value for y: ")
-    print("Exponent of x and y is equal to " + str(int(x_var) ** int(y_var)))
+    print(f'Exponent of x and y is equal to {int(x_var) ** int(y_var)}')
