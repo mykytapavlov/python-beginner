e938600948aca75eb3537307ad7411ae01410765
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-14 23:21:02 +0200

    changed print format

--- 1/sum_numbers/sum_numbers.py
+++ 1/sum_numbers/sum_numbers.py
@@ -1,4 +1,5 @@
 if __name__ == '__main__':
     x_var = input("Enter value for x: ")
     y_var = input("Enter value for y: ")
-    print("Sum of x+y is equal to " + str(int(x_var) + int(y_var)))
+    print(f'Sum of x+y is equal to {int(x_var) + int(y_var)}')
+    
