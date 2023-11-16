914a8b75a0b8be5c247bac19b9d9b664bcb617d7
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-16 15:13:55 +0200

    each third task (2d week)

--- 2/each_third/each_third.py
+++ 2/each_third/each_third.py
@@ -1,2 +1,4 @@
 if __name__ == '__main__':
-    print('Task 11. Each third')
+    list_var = input("Enter sequence of characters separated by comma: ").split(",")
+    str_var_final = [value.strip() for value in list_var]
+    print(f'Each third value in list is: {str_var_final[0::3]}')
