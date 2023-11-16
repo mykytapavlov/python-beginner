773834c3a3d1d184e1c8e3d8e65d3a5bb98d4dbe
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-16 15:38:34 +0200

    unique task (2d week)

--- 2/unique/unique.py
+++ 2/unique/unique.py
@@ -1,2 +1,6 @@
 if __name__ == '__main__':
-    print('Task 15. Unique.')
+    list_var = input("Enter sequence of characters separated by comma: ").split(",")
+    set_var = set(list_var)
+    set_var_int = lisr_var_int = [eval(i) for i in set_var]
+    print(sorted(set_var_int))
+    
