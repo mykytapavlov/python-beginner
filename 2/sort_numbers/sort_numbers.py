39c04c7e1f4c0ad490395f3127a64d14cd2d3b79
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-16 16:48:37 +0200

    corrected function which convert str to int

--- 2/sort_numbers/sort_numbers.py
+++ 2/sort_numbers/sort_numbers.py
@@ -1,4 +1,7 @@
 if __name__ == '__main__':
     list_var = input("Enter sequence of numbers separated by comma: ").split(",")
-    lisr_var_int = [eval(i) for i in list_var]
-    print(f'Sorted list: {sorted(lisr_var_int)}')
+#    lisr_var_int = [eval(i) for i in list_var]
+    list_var_int = [int(i) for i in list_var]
+#    print(f'Sorted list: {sorted(lisr_var_int)}')
+    list_var_int.sort()
+    print(f'Sorted list: {list_var_int}')
