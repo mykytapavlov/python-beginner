f551caea86699f776e332e9fe94e5542bc9e9156
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-16 23:32:30 +0200

    print of occurance of unique numbers added

--- 2/unique/unique.py
+++ 2/unique/unique.py
@@ -1,6 +1,12 @@
 if __name__ == '__main__':
     list_var = input("Enter sequence of characters separated by comma: ").split(",")
-    set_var = set(list_var)
-    set_var_int = lisr_var_int = [eval(i) for i in set_var]
-    print(sorted(set_var_int))
-    
+    list_var_int = [int(i) for i in list_var]
+    set_var = set(list_var_int)
+    print(f'Sorted list with unique values: {sorted(set_var)}')
+    cnt = dict()
+    for word in list_var_int:
+        if word in cnt:
+            cnt[word] += 1
+        else:
+            cnt[word] = 1
+    print(f'Occurrence of each unique number: {cnt}')
