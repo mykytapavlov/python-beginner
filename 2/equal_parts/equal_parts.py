739d90717992099765c046d090568156de07a56c
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-16 16:46:56 +0200

    edited printed values

--- 2/equal_parts/equal_parts.py
+++ 2/equal_parts/equal_parts.py
@@ -2,5 +2,4 @@ if __name__ == '__main__':
     list_var = input("Enter sequence of characters separated by comma: ").split(",")
     list_length = len(list_var)
     half_var = list_length // 2
-    print(f'List separated in 2 parts: {list_var[0:half_var]} and list_var[half_var:]')
- 
+    print(f'List separated in 2 parts: {list_var[0:half_var]} and {list_var[half_var:]}')
