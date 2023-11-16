255492be7110e56bb78ae3c41eec4b99d35efae8
Lil518 <149326627+Lil518@users.noreply.github.com>
2023-11-16 19:12:37 +0200

    contact (task 14)

---
+++ week_2/contact/contact.py
@@ -0,0 +1,6 @@
+x = str(input("Enter your name: "))
+y = str(input("Enter your age: "))
+z = str(input("Enter your address: "))
+w = str(input("Enter your phone: "))
+my_dict = {'name': x, 'age': y, 'address': z, 'phone': w}
+print("Contact created: ", my_dict)
