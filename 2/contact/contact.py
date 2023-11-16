744bc70d5b78955f775cdc3ae0eaf22e8d5c543b
Yuliia Spryn <yuliaspryn@gmail.com>
2023-11-16 14:41:20 +0200

    contact task (2d week)

--- 2/contact/contact.py
+++ 2/contact/contact.py
@@ -1,2 +1,4 @@
 if __name__ == '__main__':
-    print('Task 14. Contact.')
+    contact_dict = {'name': input("Enter your name: "), 'age': input("Enter your age:"),
+                    'address': input("Enter your address: "), 'phone': input("Enter your phone: ")}
+    print(f'Contact created: {contact_dict}')
