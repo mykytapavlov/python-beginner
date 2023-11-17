9f52a3f14ac296f0923a3cf8fdda23cc920e22fe
LachoLazarov <lacho.lazerov@gmail.com>
2023-11-17 17:42:09 +0200

    correction below if name = main

--- 2/contact/contact.py
+++ 2/contact/contact.py
@@ -1,11 +1,11 @@
 if __name__ == '__main__':
     print('Task 14. Contact.')
-name = input('Enter your name: ')
-name = name.capitalize()
-age = input('Enter your age: ')
-age = int(age)
-address = input('Enter your address: ')
-address = address.capitalize()
-phone = input('Enter your phone: ')
-contact_info = {'name': name, 'age':age, 'address':address, 'phone':phone}
-print('Contact created: ', contact_info)
+    name = input('Enter your name: ')
+    name = name.capitalize()
+    age = input('Enter your age: ')
+    age = int(age)
+    address = input('Enter your address: ')
+    address = address.capitalize()
+    phone = input('Enter your phone: ')
+    contact_info = {'name': name, 'age':age, 'address':address, 'phone':phone}
+    print('Contact created: ', contact_info)
--- 2/unique/unique.py
+++ 2/unique/unique.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     print('Task 15. Unique.')
-not_unique = [1, 2, 5, 4, 1, 2, 2, 3, 5, 8, 6, 7, 3, 8, 9, 6, 7, 4, 9]
-unique = list(set(not_unique))
-unique.sort()
-print(unique)
+    not_unique = [1, 2, 5, 4, 1, 2, 2, 3, 5, 8, 6, 7, 3, 8, 9, 6, 7, 4, 9]
+    unique = list(set(not_unique))
+    unique.sort()
+    print(unique)
