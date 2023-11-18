ddc2ce0153b5a0503efc663d90e1a17fd5938ef7
Denis Khomyakov <Norb@Deniss-MacBook-Pro.local>
2023-11-18 22:16:17 +0100

    contact (task 14)

--- 2/contact/contact.py
+++ 2/contact/contact.py
@@ -1,2 +1,8 @@
 if __name__ == '__main__':
     print('Task 14. Contact.')
+    name = input('Enter name: ')
+    age = int(input('Enter your age: '))
+    address = input('Enter address: ')
+    phone = input('Enter phone: ')
+    contact_hash = {'name': name, 'age': age, 'address': address, 'phone': phone}
+    print(f'Contact created: {contact_hash}')
