d675cbbe3cb10ae6f7f0109ab87fbb457ae19b48
Denis Khomyakov <Norb@Deniss-MacBook-Pro.local>
2023-11-18 22:47:15 +0100

    unique (task 15)

--- 2/unique/unique.py
+++ 2/unique/unique.py
@@ -1,2 +1,10 @@
 if __name__ == '__main__':
     print('Task 15. Unique.')
+
+    user_input = input('Enter sequence: ')
+    strings_sequence = user_input.split(',')
+    sequence = [int(item.strip()) for item in strings_sequence]
+    print(f'Result: {tuple(set(sequence))}')
+
+    occurence_number = input('Enter number to check amount of occurences: ')
+    print(sequence.count(int(occurence_number)))
