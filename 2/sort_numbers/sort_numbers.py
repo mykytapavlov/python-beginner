87c253d297ea7cd2a1dd54af5085b5f74b82dc93
Denis Khomyakov <Norb@Deniss-MacBook-Pro.local>
2023-11-18 21:20:28 +0100

    sort numbers (task 10)

--- 2/sort_numbers/sort_numbers.py
+++ 2/sort_numbers/sort_numbers.py
@@ -1,2 +1,7 @@
 if __name__ == '__main__':
     print('Task 10. Sort numbers')
+    user_input = input('Enter sequence: ')
+    strings_sequence = user_input.split(', ')
+    sequence = [int(item) for item in strings_sequence]
+    sequence.sort()
+    print(sequence)
