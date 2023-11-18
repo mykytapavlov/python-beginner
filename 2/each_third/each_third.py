6258733f53930d202d20a33a080109458ab37f66
Denis Khomyakov <Norb@Deniss-MacBook-Pro.local>
2023-11-18 21:43:51 +0100

    each third (task 11)

--- 2/each_third/each_third.py
+++ 2/each_third/each_third.py
@@ -1,2 +1,6 @@
 if __name__ == '__main__':
     print('Task 11. Each third')
+    user_input = input('Enter sequence: ')
+    strings_sequence = user_input.split(',')
+    result = [item.strip() for index, item in enumerate(strings_sequence) if (index + 1) % 3 == 0]
+    print(result)
