b65be59f0521e032a90a14765b887e02711297c0
Denis Khomyakov <Norb@Deniss-MacBook-Pro.local>
2023-11-18 22:06:13 +0100

    coordinate (task 13)

--- 2/coordinate/coordinate.py
+++ 2/coordinate/coordinate.py
@@ -1,2 +1,6 @@
 if __name__ == '__main__':
     print('Task 13. Coordinate.')
+    x = int(input('Enter x: '))
+    y = int(input('Enter y: '))
+    z = int(input('Enter z: '))
+    print(f'Coordinate ({x}, {y}, {z})')
