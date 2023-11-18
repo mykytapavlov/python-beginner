0af80929111f5285549e22680b3caac716dac5c1
Denis Khomyakov <Norb@Deniss-MacBook-Pro.local>
2023-11-18 21:54:24 +0100

    equal parts (task 12)

--- 2/equal_parts/equal_parts.py
+++ 2/equal_parts/equal_parts.py
@@ -1,2 +1,8 @@
 if __name__ == '__main__':
     print('Task 12. Equal parts')
+    user_input = input('Enter sequence: ')
+    strings_sequence = user_input.split(',')
+    sequence_middle = int(len(strings_sequence)/2)
+    first_part = strings_sequence[:sequence_middle]
+    second_part = strings_sequence[sequence_middle:]
+    print(first_part, second_part)
