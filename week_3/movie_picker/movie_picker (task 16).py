338378656e7c7d27a9d31c00c0e3d456e3e18e05
Lil518 <149326627+Lil518@users.noreply.github.com>
2023-12-01 17:01:41 +0200

    movie_picker (tasks 16) - was duplicated with task 17

--- week_3/movie_picker/movie_picker (task 16).py
+++ week_3/movie_picker/movie_picker (task 16).py
@@ -26,39 +26,27 @@ if search == 'y':
     genre_keys = list(GENRES.keys())
     print(f"Available Genres:", str(genre_keys).strip('[]'))
 
-    while True:
-        search_genre = input("Enter genre: ")
-        if search_genre in GENRES.keys():
-            print("Available movies: ", str(GENRES[search_genre]))
-            break
-        else:
-            print("Genre", search_genre, " not found. Please try again.")
-        continue
+    search_genre = input("Enter genre: ")
+    if search_genre in GENRES.keys():
+        print("Available movies: ", str(GENRES[search_genre]))
+    else:
+        print("Genre", search_genre, " not found. Please try again.")
 
-    while True:
-        movie_name_i = input("Enter movie: ")
-        if movie_name_i in GENRES.get(search_genre):
-            print("Movie to watch: ", movie_name_i, ". Genre: ", search_genre.title())
-            break
-        else:
-            print("Movie", movie_name_i, " not found. Please try again.")
-        continue
+    movie_name_i = input("Enter movie: ")
+    if movie_name_i in GENRES.get(search_genre):
+        print("Movie to watch: ", movie_name_i, ". Genre: ", search_genre.title())
+    else:
+        print("Movie", movie_name_i, " not found. Please try again.")
 else:
     search_actor = input("Search by Actor: ")
     if search_actor == 'y':
         print('Available Actors:', available_actors)
         actor_name = input("Enter actor: ")
 
-        while actor_name not in available_actors:
-            print('Actor', actor_name, 'not found. Please try again.')
-            actor_name = input('Enter actor: ')
         actor_movies = [key for key, val in CAST.items() if actor_name in val]
         print('Available movies:', actor_movies, 'with', actor_name)
         movie_name = input('Enter movie: ')
 
-        while movie_name not in actor_movies:
-            print('Movie', movie_name, 'with actor', actor_name, 'not found. Please try again.')
-            movie_watch = input('Enter movie: ')
         print('Movie to watch:', movie_name, '.', 'Starring:', CAST[movie_name], '.')
     else:
         print(f"Restart the program to search again")
