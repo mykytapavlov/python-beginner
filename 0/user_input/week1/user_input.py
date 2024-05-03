e5328183c368800000355651a807c020ced38b31
Radu <153870114+raduflorinb@users.noreply.github.com>
2024-05-03 13:15:47 +0300

    add program which prints input to console

--- 0/user_input/week1/user_input.py
+++ 0/user_input/week1/user_input.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
     print('Task 2. User`s input.')
 
-user_input = input("Enter something: ")
-print("You entered:", user_input)
+ r = input('Enter something: ')
+print(r)
