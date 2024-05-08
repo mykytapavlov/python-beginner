a5c5ccdae57f0a4900583bcc87532655dd3c2cdb
Vasyl Romanko <vromank@softserveinc.com>
2024-05-08 22:52:35 +0300

    Homework 1 Task 9 - Program that prints if letter is in word as result to console

--- 1/contains/contains.py
+++ 1/contains/contains.py
@@ -1,2 +1,8 @@
 if __name__ == '__main__':
     print('Task 9. Contains?')
+    word = input("Enter a word: ")
+    letter = input("Enter a letter: ")
+    if letter in word:
+        print("Your letter is present in your word")
+    else:
+        print("Your letter is not present in your word")
