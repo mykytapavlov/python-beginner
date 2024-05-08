3c7699e0c32c105060f39ab7e0299104cfc7e887
Vasyl Romanko <vromank@softserveinc.com>
2024-05-08 22:38:44 +0300

    Homework 1 Task 7 - Program that prints substring (from start to end) to console

--- 1/string_slice/string_slice.py
+++ 1/string_slice/string_slice.py
@@ -1,2 +1,7 @@
 if __name__ == '__main__':
     print('Task 7. String slice')
+    s = input("Enter your string: ")
+    start = int(input("Enter the start position of your substring: "))
+    end = int(input("Enter the end position of your substring: "))
+    print("Here is your substring:")
+    print(s[start:end])
