b47e308e27948c951950c7eb310b9ae9948c4c15
Vasyl Romanko <vromank@softserveinc.com>
2024-05-08 22:45:22 +0300

    Homework 1 Task 8 - Program that prints if y is greater than x

--- 1/greater/greater.py
+++ 1/greater/greater.py
@@ -1,2 +1,8 @@
 if __name__ == '__main__':
     print('Task 8. Is greater?')
+    x = int(input("Enter x: "))
+    y = int(input("Enter y: "))
+    if y > x:
+        print("y is greater than x")
+    else:
+        print("y is not greater than x")
