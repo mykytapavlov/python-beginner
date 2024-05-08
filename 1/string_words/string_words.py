b25846158d505c9d9dac2e6452cd7e7fca5c65ca
Vasyl Romanko <vromank@softserveinc.com>
2024-05-08 22:26:47 +0300

    Homework 1 Task 6 - Program that prints the words of text into console

--- 1/string_words/string_words.py
+++ 1/string_words/string_words.py
@@ -1,2 +1,7 @@
 if __name__ == '__main__':
     print('Task 6. String words')
+    text = input("Enter your text: ")
+    print("These are the words in your text:")
+    words = text.split()
+    for word in words:
+        print(word)
