a121dac37b4957b4668e7d412612a4a8e7227974
Vasyl Romanko <vromank@softserveinc.com>
2024-05-08 22:07:21 +0300

    Homework 1 Task 3 - Program that sums two numbers

--- 1/sum_numbers/sum_numbers.py
+++ 1/sum_numbers/sum_numbers.py
@@ -1,2 +1,6 @@
 if __name__ == '__main__':
     print('Task 3. Sum numbers.')
+    x = float(input("Enter first element: "))
+    y = float(input("Enter second element: "))
+    result = str(x + y)
+    print("Sum of x and y is: " + result)
