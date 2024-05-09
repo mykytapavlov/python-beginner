9b45a1d4b9a9ddc11a5e21c2f91e2de9cf20138b
Mariia Lieskova <mdakh@softserveinc.com>
2024-05-10 01:33:33 +0300

    check if letter is in word

--- 1/contains/contains.py
+++ 1/contains/contains.py
@@ -1,2 +1,8 @@
-if __name__ == '__main__':
-    print('Task 9. Contains?')
+print("Add your letter: ")
+l=str(input())
+print("Add your word: ")
+w=str(input())
+if l in w:
+    print('The word', w,  'contains letter', l)
+else:
+    print('The word', w,  'does not contain letter', l)
