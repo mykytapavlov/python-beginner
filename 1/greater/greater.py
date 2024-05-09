e0d99c236f56ef9aece64e822e59f137223e739b
Mariia Lieskova <mdakh@softserveinc.com>
2024-05-10 01:20:17 +0300

    check if number is greater

--- 1/greater/greater.py
+++ 1/greater/greater.py
@@ -1,2 +1,7 @@
-if __name__ == '__main__':
-    print('Task 8. Is greater?')
+print("Enter x: ")
+x=int(input())
+print("Enter y: ")
+y=int(input())
+if (y>x):
+    print("y is greater than x")
+else: print ('y is less than x')
