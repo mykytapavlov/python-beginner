be03c6a321e74fa5afbaded7d57de90953a8f182
Radu <153870114+raduflorinb@users.noreply.github.com>
2024-05-10 15:39:54 +0300

    add program which slice string

--- 1/string_slice/string_slice.py
+++ 1/string_slice/string_slice.py
@@ -1,2 +1,8 @@
 if __name__ == '__main__':
     print('Task 7. String slice')
+
+ s = input("enter string: ")
+ start = int (input("enter start: "))
+ end = int (input("enter end: "))
+ substring= s[start:end]
+ print (substring)
