ee9e1e5d96d6507be5fbd634ed947691571e50a8
mromaMikeR <mikhail.romanishin@gmail.com>
2024-05-11 20:24:01 +0300

    contact (task 14)

--- 2/contact/contact.py
+++ 2/contact/contact.py
@@ -1,2 +1,8 @@
-if __name__ == '__main__':
-    print('Task 14. Contact.')
+user_info = {}
+
+user_info.update({'name': input('Please provide your name: ')})
+user_info.update({'age': input('Please provide your age: ')})
+user_info.update({'address': input('Please provide your address: ')})
+user_info.update({'phone': input('Please provide your phone: ')})
+
+print(user_info)
