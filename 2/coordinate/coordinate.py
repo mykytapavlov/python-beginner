1130277e37e19b51e7dc1a4cc7d3cea0b6ac3674
Vasyl Romanko <vromank@softserveinc.com>
2024-05-14 22:48:13 +0300

    Homework 2 Task 13 - return a coordinate like (x, y, z) to console

--- 2/coordinate/coordinate.py
+++ 2/coordinate/coordinate.py
@@ -1,2 +1,7 @@
 if __name__ == '__main__':
     print('Task 13. Coordinate.')
+    x = int(input("Input `x` :"))
+    y = int(input("Input `y` :"))
+    z = int(input("Input `z` :"))
+    coordinates = (x,y,z)
+    print("Output: Coordinate", coordinates)
