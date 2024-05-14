41c734b3ae83904673e57cd42426bd4f3e0105ef
Vasyl Romanko <vromank@softserveinc.com>
2024-05-14 22:43:02 +0300

    Homework 2 Task 12 - split sequence on 2 (mostly) equal parts

--- 2/equal_parts/equal_parts.py
+++ 2/equal_parts/equal_parts.py
@@ -1,2 +1,6 @@
 if __name__ == '__main__':
     print('Task 12. Equal parts')
+    s = input("Input your sequence of characters separated by ', ': ")
+    sequence = s.split(', ')
+    print("Your sequence split it on 2 (mostly) equal parts:")
+    print(sequence[:len(sequence)//2], sequence[len(sequence)//2:])
