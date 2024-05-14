cc85f66336f319e5e671df58ca328600fc8347be
Vasyl Romanko <vromank@softserveinc.com>
2024-05-14 22:00:40 +0300

    Homework 2 Task 10 - print sorted list of numbers to console

--- 2/sort_numbers/sort_numbers.py
+++ 2/sort_numbers/sort_numbers.py
@@ -1,2 +1,7 @@
 if __name__ == '__main__':
     print('Task 10. Sort numbers')
+    s = input("Input your list of numbers separated by ', ': ")
+    numbers = [int(a) for a in s.split(', ')]
+    numbers.sort()
+    print("Sorted list:")
+    print(numbers)
