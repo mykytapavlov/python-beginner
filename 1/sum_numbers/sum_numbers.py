f2a04aa4a5ff917f2245964c280c14300fb82154
Vasyl Romanko <vromank@softserveinc.com>
2024-05-14 19:42:53 +0300

    Homework 1 Task 3 - Slight changes to print format

--- 1/sum_numbers/sum_numbers.py
+++ 1/sum_numbers/sum_numbers.py
@@ -2,5 +2,5 @@ if __name__ == '__main__':
     print('Task 3. Sum numbers.')
     x = float(input("Enter first element: "))
     y = float(input("Enter second element: "))
-    result = str(x + y)
-    print("Sum of x and y is: " + result)
+    result = x + y
+    print("Sum of x and y is: ", result)
