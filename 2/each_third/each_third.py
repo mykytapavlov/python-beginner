4f4191e49b6723da98960d9962409a3bd3694e5c
Vasyl Romanko <vromank@softserveinc.com>
2024-05-14 22:17:57 +0300

    Homework 2 Task 11 - print list of each third element to console

--- 2/each_third/each_third.py
+++ 2/each_third/each_third.py
@@ -1,2 +1,6 @@
 if __name__ == '__main__':
     print('Task 11. Each third')
+    s = input("Input your sequence of characters separated by ', ': ")
+    sequence = s.split(', ')
+    print("List of each third element:")
+    print(sequence[2::3])
