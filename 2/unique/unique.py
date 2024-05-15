18c5fac0ab1bdcbead01a23ef2fe82ed59ca7963
Vasyl Romanko <vromank@softserveinc.com>
2024-05-15 21:03:57 +0300

    Homework 2 Task 15 - return sequence of sorted unique numbers to console

--- 2/unique/unique.py
+++ 2/unique/unique.py
@@ -1,2 +1,10 @@
 if __name__ == '__main__':
     print('Task 15. Unique.')
+    s = input("Enter your sequence of numbers: ")
+    s.replace(" ", "")
+    numbers = [int(a) for a in s.split(',')]
+    unique = list(set(numbers))
+    unique.sort()
+    print(f"Unique numbers from your sequence: {unique}")
+    for i in range(len(unique)):
+        print(f"Number {unique[i]} occurs in your sequence {numbers.count(unique[i])} times")
