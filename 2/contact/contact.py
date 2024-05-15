1b5a863e6d44bf6766fb5e8ce68c78ce6d8c5334
Vasyl Romanko <vromank@softserveinc.com>
2024-05-15 20:44:30 +0300

    Homework 2 Task 14 - return contact info

--- 2/contact/contact.py
+++ 2/contact/contact.py
@@ -1,2 +1,8 @@
 if __name__ == '__main__':
     print('Task 14. Contact.')
+    contact = dict()
+    contact['name'] = input("Enter your name: ")
+    contact['age'] = int(input("Enter your age: "))
+    contact['address'] = input("Enter your address: ")
+    contact['phone'] = int(input("Enter your phone in format 0670000000: "))
+    print(f"Contact created: {contact}")
