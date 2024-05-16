84568cc8faf5696e3a7b04b59e0da9cd904f7892
“MikeR” <mikhail.romanishin@gmail.com>
2024-05-16 18:35:43 +0300

    unique (task 15)

--- 2/unique/unique.py
+++ 2/unique/unique.py
@@ -1,2 +1,9 @@
-if __name__ == '__main__':
-    print('Task 15. Unique.')
+values_list = []
+user_input = values_list.append(int(input('Enter a character1: ')))
+user_input2 = values_list.append(int(input('Enter a character2: ')))
+user_input3 = values_list.append(int(input('Enter a character3: ')))
+user_input4 = values_list.append(int(input('Enter a character4: ')))
+user_input5 = values_list.append(int(input('Enter a character5: ')))
+user_input6 = values_list.append(int(input('Enter a character6: ')))
+sorted_list = sorted(set(values_list))
+print('Unique: ', sorted_list)
