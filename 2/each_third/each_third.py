15470d3d43ad1ab64b93947681a21810a9ec1555
“MikeR” <mikhail.romanishin@gmail.com>
2024-05-16 11:52:44 +0300

    each third (task 11)

--- 2/each_third/each_third.py
+++ 2/each_third/each_third.py
@@ -1,2 +1,12 @@
-if __name__ == '__main__':
-    print('Task 11. Each third')
+values_list = []
+user_input = values_list.append(input('Enter a character1: '))
+user_input2 = values_list.append(input('Enter a character2: '))
+user_input3 = values_list.append(input('Enter a character3: '))
+user_input4 = values_list.append(input('Enter a character4: '))
+user_input5 = values_list.append(input('Enter a character5: '))
+user_input6 = values_list.append(input('Enter a character6: '))
+user_input7 = values_list.append(input('Enter a character7: '))
+user_input8 = values_list.append(input('Enter a character8: '))
+every_third = values_list[::2]
+print(values_list)
+print(every_third)
