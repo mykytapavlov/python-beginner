3193ed651721bfcd4e84203345fb9f81803310a8
Mariia Lieskova <mdakh@softserveinc.com>
2024-05-17 13:20:33 +0300

    equal parts (task 12)

--- 2/equal_parts/equal_parts.py
+++ 2/equal_parts/equal_parts.py
@@ -1,2 +1,6 @@
 if __name__ == '__main__':
     print('Task 12. Equal parts')
+list = input("Enter elements: ").split()
+list_1=list[:len(list)//2]
+list_2=list[len(list)//2:]
+print("First part: ", list_1,"\n","Second part: ", list_2)
