34d35777dcd8e9d42c090dd7808d7409fd84fc44
Radu <153870114+raduflorinb@users.noreply.github.com>
2024-05-17 13:03:52 +0300

    sort numbers (task 10)

--- 2/sort_numbers/sort_numbers.py
+++ 2/sort_numbers/sort_numbers.py
@@ -1,2 +1,7 @@
 if __name__ == '__main__':
     print('Task 10. Sort numbers')
+
+s = input("Enter a sequence of numbers separated by commas: ")
+numbers = [int(x.strip()) for x in s.split(',')]
+sorted_numbers = sorted(numbers)
+print("Output:", sorted_numbers)
