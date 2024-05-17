832f4e27f3c937017cad82205633309a0f06d67b
Radu <153870114+raduflorinb@users.noreply.github.com>
2024-05-17 13:32:20 +0300

    each third (task 11)

--- 2/each_third/each_third.py
+++ 2/each_third/each_third.py
@@ -1,2 +1,6 @@
 if __name__ == '__main__':
     print('Task 11. Each third')
+s  = input("Enter a sequence of characters separated by commas: ")
+characters = [x.strip() for x in s.split(',')]
+third_elements = characters[::3]
+print(third_elements)
