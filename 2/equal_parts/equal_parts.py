62a1c689bf20a7fb356395d02b4808bebe641d19
Radu <153870114+raduflorinb@users.noreply.github.com>
2024-05-17 13:06:54 +0300

    equal parts (task 12)

--- 2/equal_parts/equal_parts.py
+++ 2/equal_parts/equal_parts.py
@@ -1,2 +1,9 @@
 if __name__ == '__main__':
     print('Task 12. Equal parts')
+   
+s = input("Enter a sequence of characters separated by commas: ")
+characters = [x.strip() for x in s.split(',')]
+midpoint = (len(characters) + 1) // 2
+first_part = characters[:midpoint]
+second_part = characters[midpoint:]
+print(first_part, second_part)
