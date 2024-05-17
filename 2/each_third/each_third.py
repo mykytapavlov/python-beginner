34df541a1a8dbb77542820cf90fc4ffa10caa67f
mromaMikeR <mikhail.romanishin@gmail.com>
2024-05-17 15:23:10 +0300

    each third (task 11) optimization

--- 2/each_third/each_third.py
+++ 2/each_third/each_third.py
@@ -1,17 +1,4 @@
-values_list = []
-user_input = values_list.append(input('Enter a character1: '))
-user_input2 = values_list.append(input('Enter a character2: '))
-user_input3 = values_list.append(input('Enter a character3: '))
-user_input4 = values_list.append(input('Enter a character4: '))
-user_input5 = values_list.append(input('Enter a character5: '))
-user_input6 = values_list.append(input('Enter a character6: '))
-user_input7 = values_list.append(input('Enter a character7: '))
-user_input8 = values_list.append(input('Enter a character8: '))
+values_list = input('Enter some characters: ').split()
 every_third = values_list[::2]
 print(values_list)
 print(every_third)
-
-# or
-# values_list = [3, 2, 1, 4, 5, 2, 'ss', 'q']
-# every_third = values_list[::2]
-# print(every_third)
