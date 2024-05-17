cc500cf9e874fff0dde9a55bb390bebc2aaa195f
Mariia Lieskova <mdakh@softserveinc.com>
2024-05-17 15:49:59 +0300

    unique (task 15)

--- 2/unique/unique.py
+++ 2/unique/unique.py
@@ -1,2 +1,10 @@
 if __name__ == '__main__':
     print('Task 15. Unique.')
+from collections import Counter
+list =input("Enter elements: ").split()
+l=sorted(set(list))
+print("The unique list is: ", l)
+
+
+
+
