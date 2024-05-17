ecad4c2aa0d96128819ab4b871cef58c9b378e86
Mariia Lieskova <mdakh@softserveinc.com>
2024-05-17 12:34:51 +0300

    contact(task 14)

--- 2/contact/contact.py
+++ 2/contact/contact.py
@@ -1,2 +1,8 @@
 if __name__ == '__main__':
     print('Task 14. Contact.')
+dict=dict()
+dict["Name"]=input('Enter your name: ')
+dict["Age"]=input('Enter your age: ')
+dict["Address"]=input('Enter your address ')
+dict["Phone"]=input('Enter your phone ')
+print("Contact created:", dict)
