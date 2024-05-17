1b3b3a07f554093d9927dd4fcca536dd9de1bf59
Radu <153870114+raduflorinb@users.noreply.github.com>
2024-05-17 13:33:53 +0300

    coordinate (task 13)

--- 2/coordinate/coordinate.py
+++ 2/coordinate/coordinate.py
@@ -1,2 +1,6 @@
 if __name__ == '__main__':
     print('Task 13. Coordinate.')
+x = int(input("Input 'x': "))
+y = int(input("Input 'y': "))
+z = int(input("Input 'z': "))
+print(f"Output: Coordinate ({x}, {y}, {z})")
