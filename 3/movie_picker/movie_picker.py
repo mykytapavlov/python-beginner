27219d7d53365ba6c1ae976e8ec7e81f9c365176
mromaMikeR <mikhail.romanishin@gmail.com>
2024-05-18 14:01:44 +0300

    movie_picker (task 16)

--- 3/movie_picker/movie_picker.py
+++ 3/movie_picker/movie_picker.py
@@ -1,2 +1,40 @@
-if __name__ == '__main__':
-    print('Tasks 15-17. Movie picker.')
+GENRES = {
+    'comedy': ['Meet the Parents', 'Anger Management'],
+    'adventures': ['Mummy'],
+    'romantic': ['Vanilla Sky', 'Meet Joe Black'],
+    'drama': ['Meet Joe Black'],
+    'thriller': ['Vanilla Sky'],
+    'action': ['Mission Impossible']
+}
+ACTORS = {
+    'Robert De Niro': ['Meet the Parents'],
+    'Ben Stiller': ['Meet the Parents'],
+    'Adam Sandler': ['Anger Management'],
+    'Jack Nicholson': ['Anger Management'],
+    'Brendan Fraser': ['Mummy'],
+    'Rachel Weisz': ['Mummy'],
+    'Tom Cruise': ['Vanilla Sky', 'Mission Impossible'],
+    'Penelope Cruz': ['Vanilla Sky'],
+    'Cameron Diaz': ['Vanilla Sky'],
+    'Brad Pitt': ['Meet Joe Black'],
+    'Anthony Hopkins': ['Meet Joe Black'],
+    'Jeremy Renner': ['Mission Impossible']
+}
+
+user_input = input('Enter Genre or Actor: ')
+
+for genre in GENRES:
+    i = 0
+    while i < len(GENRES):
+        i += 1
+        if user_input in genre:
+            print(genre, ' :', GENRES.get(genre))
+            break
+
+for actor in ACTORS:
+    i = 0
+    while i < len(ACTORS):
+        i += 1
+        if user_input in actor:
+            print(actor, ' :', ACTORS.get(actor))
+            break
