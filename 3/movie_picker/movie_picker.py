31d143b5ac91eca44e8da5bbf1c8f3b3492f460c
mromaMikeR <mikhail.romanishin@gmail.com>
2024-05-22 11:07:28 +0300

    movie_picker (task 17)

--- 3/movie_picker/movie_picker.py
+++ 3/movie_picker/movie_picker.py
@@ -1,3 +1,5 @@
+from typing import List
+
 GENRES = {
     'comedy': ['Meet the Parents', 'Anger Management'],
     'adventures': ['Mummy'],
@@ -28,7 +30,74 @@ CAST = {
     'Meet Joe Black': ['Brad Pitt', 'Anthony Hopkins'],
     'Mission Impossible': ['Tom Cruise', 'Jeremy Renner']
 }
+# task 17
+user_input_genre = input('Search by Genre y/n: ')
+if user_input_genre == 'y':
+    print('Available Genres: ', list(GENRES.keys()))
 
+    enter_genre_input = input('Enter genre: ')
+    # when user's genre input doesn't match any of genres in GENRES, it goes back and asks to provide a genre again
+    while enter_genre_input not in GENRES.keys():
+        print('Genre', enter_genre_input, 'not found. Please try again.')
+        enter_genre_input = input('Enter genre: ')
+    # if user's input is the same as we have in GENRES, we move forward
+    else:
+        # and display all movies for the entered genre
+        for genre in GENRES.keys():
+            if enter_genre_input in genre:
+                print('Available Movies: ', GENRES.get(genre))
+                enter_input_movie = input('Enter movie: ')
+                # when user's movie input doesn't match any of movie from the list, it goes back and asks to provide a movie again
+                while enter_input_movie not in GENRES.get(genre):
+                    print('Movie', enter_input_movie, 'not found. Please try again.')
+                    enter_input_movie = input('Enter movie: ')
+                else:
+                    # if movie exists in the list, we move on and display it as a movie to watch with name and genre. Program ends
+                    for movie in GENRES.get(genre):
+                        if enter_input_movie in movie:
+                            print('Movie to watch:', movie + '.', 'Genre:', enter_genre_input + '.')
+
+if user_input_genre == 'n':
+    user_input_actor = input('Search by Actor y/n: ')
+    # when user selects yes for actors we display all available actors
+    if user_input_actor == 'y':
+        available_actors = []
+        # And actors shouldn't repeat
+        for actor in CAST.values():
+            for x in actor:
+                if x not in available_actors:
+                    available_actors.append(x)
+        print('Available Actors:', available_actors)
+
+    enter_actor_input = input('Enter actor: ')
+    # pretty the same list if available actors with which we compare user's actor input
+    more_available_actors = []
+    for actor in CAST.values():
+        for x in actor:
+            if x not in more_available_actors:
+                more_available_actors.append(x)
+    # when user's actor input doesn't match any of actors in CAST, it goes back and asks to provide an actor again
+    while enter_actor_input not in more_available_actors:
+        print('Actor', enter_actor_input, 'not found. Please try again.')
+        enter_actor_input = input('Enter actor: ')
+    else:
+        # if user's actor input is the same as we have in CAST, we move forward and display all available movies for the chosen actor
+        available_movies = []
+        for another_movie, another_actor in CAST.items():
+            if enter_actor_input in another_actor:
+                available_movies.append(another_movie)
+        print('Available movies:', available_movies, 'with', enter_actor_input)
+        enter_movie_input = input('Enter movie: ')
+        # when user's movie input doesn't match any of movie from the list, it goes back and asks to provide a movie again
+        while enter_movie_input not in available_movies:
+            print('Movie', enter_movie_input, 'not found. Please try again.')
+            enter_movie_input = input('Enter movie: ')
+
+        else:
+            # if movie exists in the list, we move on and display it as a movie to watch with name and actor. Program ends
+            print('Movie to watch:', enter_movie_input + '.', 'Starring:', enter_actor_input + '.')
+
+'''
 # task 16
 user_input_genre = input('Search by Genre y/n: ')
 if user_input_genre == 'y':
@@ -59,7 +128,7 @@ if user_input_genre == 'n':
     for different_movie in available_movies:
         if enter_movie_input in different_movie:
             print('Movie to watch:', different_movie + '.', 'Starring:', enter_actor_input + '.')
-
+'''
 # task 15
 '''
 user_input_genre = input('Search by Genre y/n: ')
