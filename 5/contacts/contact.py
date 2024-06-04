6fc5141757d80b4e5d60f9e419f6a0820d1b6bd0
Vasyl Romanko <vromank@softserveinc.com>
2024-06-04 03:40:45 +0300

    Homework 5 Task 19 - Part 1 (reworked Contact class)

--- 5/contacts/contact.py
+++ 5/contacts/contact.py
@@ -1,30 +1,47 @@
-contact = {
-    'name': None,
-    'email': None,
-    'age': None
-}
-
-
-def validate_name(name):
-    if len(name) > 50:
-        raise ValueError('Name is too large!')
-
-
-def validate_email(email):
-    if '@' not in email or '.' not in email:
-        raise ValueError('Invalid email!')
-
-
-def validate_age(age):
-    try:
-        age = int(age)  # Python will raise ValueError if not numeric
-        if age <= 0:
-            # We ask Python to raise ValueError if <= 0
-            raise ValueError
-    except ValueError:
-        raise ValueError('Invalid age!')
-
-
 class Contact:
-    # TODO: implement instead of above
-    ...
+    def __init__(self,name,email,age):
+        self.name = name
+        self.email = email
+        self.age = age
+
+    @classmethod
+    def create_from_input(cls):
+        name = input('Name: ')
+        cls.validate_name(name)
+        email = input('Email: ')
+        cls.validate_email(email)
+        age = input('Age: ')
+        cls.validate_age(age)
+        return cls(name, email, age)
+
+    @staticmethod
+    def validate_name(name):
+        if len(name) > 50:
+            raise ValueError('Name is too large!')
+
+    @staticmethod
+    def validate_email(email):
+        if '@' not in email or '.' not in email:
+            raise ValueError('Invalid email!')
+
+    @staticmethod
+    def validate_age(age):
+        try:
+            age = int(age)  # Python will raise ValueError if not numeric
+            if age <= 0:
+                # We ask Python to raise ValueError if <= 0
+                raise ValueError
+        except ValueError:
+            raise ValueError('Invalid age!')
+
+    def __str__(self):
+        output = ''
+        output = (
+                output
+                + '-' * 10 + '\n'
+                + 'Name:' + self.name + '\n'
+                + 'Email:' + self.email + '\n'
+                + 'Age:' + str(self.age) + '\n'
+                + '-' * 10
+        )
+        return output
--- 5/contacts/main.py
+++ 5/contacts/main.py
@@ -1,4 +1,4 @@
-from contact import contact, validate_name, validate_age, validate_email
+from contact import Contact
 
 
 if __name__ == '__main__':
@@ -8,26 +8,12 @@ if __name__ == '__main__':
 
     while True:
         try:
-            name = input('name: ')
-            validate_name(name)
-
-            email = input('email: ')
-            validate_email(email)
-
-            age = input('age: ')
-            validate_age(age)
+            new_contact = Contact.create_from_input()
         except ValueError as e:
             print(e)
             continue
 
-        new_contact = contact.copy()
-        new_contact['name'] = name
-        new_contact['email'] = email
-        new_contact['age'] = age
-
-        print('-' * 10)
         print(new_contact)
-        print('-' * 10)
 
         contact_list.append(new_contact)
         proceed = input('add another one? (y/n): ')
