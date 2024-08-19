d9baec5d960fd9513ac9421b2cf86bca0d9ec346
Kateryna Borysova <kbory@softserveinc.com>
2024-08-19 16:03:28 +0300

    add program which sums two numbers

--- 1/sum_numbers/sum_numbers.py
+++ 1/sum_numbers/sum_numbers.py
@@ -1,2 +1,5 @@
-if __name__ == '__main__':
-    print('Task 3. Sum numbers.')
+x = 12
+y = 0.56
+result = x + y
+print('Sum x and y is', result)
+print("X is:", type(x), "Y is type:", type(y), "Result is:", type(result))
