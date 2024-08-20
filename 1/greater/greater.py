80de2e0b1a18695820225c766923d45aac6bfb38
Kateryna Borysova <kbory@softserveinc.com>
2024-08-20 15:43:36 +0300

    check if number is greater

--- 1/greater/greater.py
+++ 1/greater/greater.py
@@ -1,2 +1,10 @@
-if __name__ == '__main__':
-    print('Task 8. Is greater?')
+x = 47.88
+y = 88.65
+print("X =", (x))
+print("Y =", (y))
+if x > y:
+    print("X is grater then Y")
+else:
+    print("Y is grater then X")
+print("X as result" if y > x else " ") # in line conditional
+
