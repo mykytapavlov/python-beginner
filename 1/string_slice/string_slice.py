85e188dbe7f9bf44f055c3e8d09210c9d4086ff5
Kateryna Borysova <kbory@softserveinc.com>
2024-08-20 12:01:48 +0300

    slice string

--- 1/string_slice/string_slice.py
+++ 1/string_slice/string_slice.py
@@ -1,2 +1,9 @@
-if __name__ == '__main__':
-    print('Task 7. String slice')
+
+s = 'Pythons are some of the largest snakes in the world'
+startIndex = 32
+endIndex = 36
+# stepIndex = 1
+result = s[startIndex:endIndex + 1]
+result = result.capitalize()
+
+print(result)
