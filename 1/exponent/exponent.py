e8028dbdc54d7e2f34cd06641ef1a2e36f6a6d5f
SerhiiQA45 <dim.danov@gmail.com>
2024-08-26 19:04:16 +0300

    add program which returns exponent of two numbers

--- 1/exponent/exponent.py
+++ 1/exponent/exponent.py
@@ -1,2 +1,8 @@
+from math import exp
+
 if __name__ == '__main__':
-    print('Task 4. Exponent.')
+    x = 3
+    y = 2
+#    w = x ** y
+#    print('result =', w)
+    print('result = ', (x ** y))
