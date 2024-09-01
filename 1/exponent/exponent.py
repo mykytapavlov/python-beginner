3ab41917f49a2ebf819c00587ef5066610d4a7ca
SerhiiQA45 <dim.danov@gmail.com>
2024-09-01 20:57:30 +0300

    add program which returns exponent of two numbers - corrections

--- 1/exponent/exponent.py
+++ 1/exponent/exponent.py
@@ -1,8 +1,10 @@
-from math import exp
 
 if __name__ == '__main__':
-    x = 3
+#    x = input('Enter the basis of the degree: ')
+#    y = input('Enter a degree: ')
+    x = 6
     y = 2
 #    w = x ** y
 #    print('result =', w)
-    print('result = ', (x ** y))
+#    print('result = ', x ** y)
+    print(f'result = {x ** y}')
