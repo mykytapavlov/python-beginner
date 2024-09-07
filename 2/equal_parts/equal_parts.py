0c10940f58f3118b00414c11e2bdddc430efcdab
SerhiiQA45 <dim.danov@gmail.com>
2024-09-07 21:07:59 +0300

    equal parts (task 12)

--- 2/equal_parts/equal_parts.py
+++ 2/equal_parts/equal_parts.py
@@ -1,2 +1,16 @@
 if __name__ == '__main__':
-    print('Task 12. Equal parts')
+    import numpy as np
+
+    my_list = [3, 2, 1, 4, 5,  2, 'ss', 'q']
+    new_list = np.array_split(my_list, 2)
+
+
+
+
+#    def chunks(iterable, size):
+#        iterator = iter(iterable)
+
+#    my_list = [3, 2, 1, 4, 5,  2, 'ss', 'q']
+#    new_list = list(chunks(my_list, 2))
+
+    print(new_list)
