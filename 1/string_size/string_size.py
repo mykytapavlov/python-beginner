1128cee33d34212c0dceb65860c04446f25705ef
SerhiiQA45 <dim.danov@gmail.com>
2024-09-07 20:33:02 +0300

    add program which prints size of an input string - corrections

--- 1/string_size/string_size.py
+++ 1/string_size/string_size.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
 
-    s = 'whatever you want, whatever you need'
+    s = input('Please neter some phrase: ')
     m = len (s)
 
     print('String size is', m, 'symbols')
