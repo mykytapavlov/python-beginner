1350497f9d92e68aa0d6d33bdeb59b5922de8bb4
SerhiiQA45 <dim.danov@gmail.com>
2024-09-07 22:13:18 +0300

    unique (task 15)

--- 2/unique/unique.py
+++ 2/unique/unique.py
@@ -1,2 +1,13 @@
 if __name__ == '__main__':
-    print('Task 15. Unique.')
+
+    list_init = [1, 2, 11, 1, 2, 2, 3]
+    res_list = []
+
+    for item in list_init:
+        if item not in res_list:
+            res_list.append(item)
+
+    res_list.sort()
+
+
+    print('Unique: ', res_list)
