4ff030b30e43016f0753e278f411b7534e8aade1
SerhiiQA45 <dim.danov@gmail.com>
2024-09-07 21:00:09 +0300

    each third (task 11)

--- 2/each_third/each_third.py
+++ 2/each_third/each_third.py
@@ -1,2 +1,16 @@
 if __name__ == '__main__':
-    print('Task 11. Each third')
+
+    my_list = [3, 2, 1, 4, 5,  2, 'ss', 'q']
+    x = my_list[0:8:2]
+
+    print(x)
+
+
+
+
+#    def every_nth(x, nth):
+#        return x[nth -1::nth]
+
+#    x = [3, 2, 1, 4, 5,  2, 'ss', 'q']
+
+#    print(every_nth(x, 3))
