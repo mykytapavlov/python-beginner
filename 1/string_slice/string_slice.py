2850898b3b7964a0d1f432f9ea3dd974c67d3375
SerhiiQA45 <dim.danov@gmail.com>
2024-09-07 20:27:42 +0300

    add program which slice string

--- 1/string_slice/string_slice.py
+++ 1/string_slice/string_slice.py
@@ -1,5 +1,10 @@
 if __name__ == '__main__':
 
-    input_string = 'when there is so much left to do'
-    res = input_string.split()
-    print(res)
+    input_string = input('Please enter some phrase: ')
+    a = input('Please enter start index: ')
+    a = int(a)
+    b = input('Please enter end index: ')
+    b = int(b)
+    print(input_string[a:b])
+
+
