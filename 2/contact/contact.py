89ae50d8f4734ebde6a74f8c77b83428b1b1a132
SerhiiQA45 <dim.danov@gmail.com>
2024-09-07 21:56:01 +0300

    contact (task 14)

--- 2/contact/contact.py
+++ 2/contact/contact.py
@@ -1,2 +1,19 @@
 if __name__ == '__main__':
-    print('Task 14. Contact.')
+
+    name = input('Please enter your name: ')
+    age = input('Please enter your age: ')
+    address = input('Please enter your address: ')
+    phone = input('Please enter your phone: ')
+
+#    contact_data = (name, age, address, phone)
+#    contact_keys = ('name', 'age', 'address', 'phone')
+#    contact = dict.fromkeys(contact_keys, name)
+
+    contact = {}
+    contact['name'] = name
+    contact['age'] = age
+    contact['address'] = address
+    contact['phone'] = phone
+
+
+    print('Contact created: ', contact)
