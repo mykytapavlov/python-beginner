5a0cdcd4ab9b8603f7ae12797fda2d20e12b320a
SerhiiQA45 <dim.danov@gmail.com>
2024-09-12 19:36:55 +0300

    movie picker tasks 15-17

--- 3/movie_picker/movie_picker.py
+++ 3/movie_picker/movie_picker.py
@@ -1,2 +1,43 @@
 if __name__ == '__main__':
-    print('Tasks 15-17. Movie picker.')
+
+    search_criteria = input('Please enter search criteria - genre or actor: ')
+
+    selected_genre = None
+    selected_actor = None
+
+    GENRES = {
+        'comedy': ['Meet the Parents', 'Anger Management'],
+        'adventures': ['Mummy'],
+        'romantic': ['Vanilla Sky', 'Meet Joe Black'],
+        'drama': ['Meet Joe Black'],
+        'thriller': ['Vanilla Sky'],
+        'action': ['Mission Impossible']
+    }
+
+    ACTORS = {
+        'Robert De Niro': ['Meet the Parents'],
+        'Ben Stiller': ['Meet the Parents'],
+        'Adam Sandler': ['Anger Management'],
+        'Jack Nicholson': ['Anger Management'],
+        'Brendan Fraser': ['Mummy'],
+        'Rachel Weisz': ['Mummy'],
+        'Tom Cruise': ['Vanilla Sky', 'Mission Impossible'],
+        'Penelope Cruz': ['Vanilla Sky'],
+        'Cameron Diaz': ['Vanilla Sky'],
+        'Brad Pitt': ['Meet Joe Black'],
+        'Anthony Hopkins': ['Meet Joe Black'],
+        'Jeremy Renner': ['Mission Impossible']
+    }
+
+    if search_criteria == 'genre':
+        selected_genre = input('Please enter a genre: \ncomedy \nadventures \nromantic \ndrama \nthriller \naction \n ')
+        movies_by_genre = GENRES.get(selected_genre)
+        print('Available movies: ', movies_by_genre)
+    elif search_criteria == 'actor':
+        selected_actor = input('Please enter an actor: \nRobert De Niro \nBen Stiller \nAdam Sandler \nJack Nicholson \nBrendan Fraser \nRachel Weisz \nTom Cruise \nPenelope Cruz \nCameron Diaz \nBrad Pitt \nAnthony Hopkins \nJeremy Renner \n ')
+        movies_by_actor = ACTORS.get(selected_actor)
+        print('Available movies: ', movies_by_actor)
+    else:
+        print('No such a search criteria')
+
+
---
+++ 3/movie_picker/movie_picker_3.py
@@ -0,0 +1,87 @@
+from orca.keynames import getKeyName
+
+if __name__ == '__main__':
+
+    search_by_genre = input('Search by genre - print y or n: ')
+
+    search_criteria = None
+    selected_genre = None
+    selected_actor = None
+
+    GENRES = {
+        'comedy': ['Meet the Parents', 'Anger Management'],
+        'adventures': ['Mummy'],
+        'romantic': ['Vanilla Sky', 'Meet Joe Black'],
+        'drama': ['Meet Joe Black'],
+        'thriller': ['Vanilla Sky'],
+        'action': ['Mission Impossible']
+    }
+
+    CAST = {
+        'Meet the Parents': ['Robert De Niro', 'Ben Stiller'],
+        'Anger Management': ['Adam Sandler', 'Jack Nicholson'],
+        'Mummy': ['Brendan Fraser', 'Rachel Weisz'],
+        'Vanilla Sky': ['Tom Cruise', 'Penelope Cruz', 'Cameron Diaz'],
+        'Meet Joe Black': ['Brad Pitt', 'Anthony Hopkins'],
+        'Mission Impossible': ['Tom Cruise', 'Jeremy Renner']
+    }
+
+    if search_by_genre == 'y':
+        search_criteria = 'genre'
+    elif search_by_genre == 'n':
+        search_by_actor = input('Search by actor - print y or n: ')
+        if search_by_actor == 'y':
+            search_criteria = 'actor'
+        elif search_by_actor == 'n':
+            exit()
+        else:
+            print('Unknown input')
+    else:
+        print('Unknown input')
+
+    if search_criteria == 'genre':
+        available_genres = list(GENRES.keys())
+        print('Available genres: ', available_genres)
+        selected_genre = input('Please enter a genre: ')
+        while selected_genre not in available_genres:
+            print(f'Genre {selected_genre} not found. Please try again.')
+            selected_genre = input('Please enter a genre: ')
+        movies_by_genre = GENRES.get(selected_genre)
+        print('Available movies: ', movies_by_genre)
+        selected_movie = input('Please enter movie: ')
+        while selected_movie not in movies_by_genre:
+            print(f'Movie {selected_movie} not found. Please try again.')
+            selected_movie = input('Please enter movie: ')
+        print(f'Movie to watch: {selected_movie}. Genre: {selected_genre}.')
+    elif search_criteria == 'actor':
+        actors = list(CAST.values())
+        actors_list = sum(actors, [])
+        unique_actors_list = []
+        for item in actors_list:
+            if item not in unique_actors_list:
+                unique_actors_list.append(item)
+        print('Available actors: ', unique_actors_list)
+        selected_actor = input('Please enter actor: ')
+        while selected_actor not in unique_actors_list:
+            print(f'Actor {selected_actor} not found. Please try again.')
+            selected_actor = input('Please enter actor: ')
+        res_movie_list = []
+        for movie, actors_list in CAST.items():
+            if selected_actor in actors_list:
+                res_movie_list.append(movie)
+        print('Available movies: ', res_movie_list, 'with', selected_actor)
+        selected_movie = input('Please enter movie: ')
+        while selected_movie not in res_movie_list:
+            print(f'Movie {selected_movie} with {selected_actor} not found. Please try again.')
+            selected_movie = input('Please enter movie: ')
+        print(f'Movie to watch: {selected_movie}. Starring: {selected_actor}')
+
+
+
+
+
+
+
+
+
+
